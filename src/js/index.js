c0356b28302bf44a163580522bf905cdee0cb7a5
polnolunie <polnolunie255@gmail.com>
2026-05-07 21:37:01 +0900

    input styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
-import '../scss/style.scss'
+const debounce = (fn, debounceTime = 0) => {
+  // your code goes here
+  let timerId
+  return function (...args) {
+    const context = this
+    clearTimeout(timerId)
+    timerId = setTimeout(() => {
+      fn.apply(context, args)
+    }, debounceTime)
+  }
+}
 
-console.log('It works!')
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('input')
+  const label = document.querySelector('.pressEnter')
+
+  if (input && label) {
+    input.addEventListener('input', () => {
+      if (input.value.length > 0) {
+        label.style.display = 'flex'
+      } else {
+        label.style.display = 'none'
+      }
+    })
+  }
+})
